0249c4cb063dd8683696aa648ff9061b65792762
ituki <82156802+ituki0426@users.noreply.github.com>
2023-02-14 07:35:56 +0000

    前処理の実装

---
+++ src/augmentation.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, imgn, boxes=None, labels=None):
+        for t in self.transforms:
+            imgn, boxes, labels = t(imgn, boxes, labels)
+        return imgn, boxes, labels
+
+
+class ConvertFromInts(object):
+    def __call__(self, image, boxes=None, labels=None):
+        return image.astype(np.float32), boxes, labels
+
+
+class ToAbsoluteCoords(object):
+    def __call__(self, image, boxes=None, labels=None):
+        height, width, channels = image.shape
+        boxes[:, 0] *= width
+        boxes[:, 2] *= width
+        boxes[:, 1] *= height
+        boxes[:, 3] *= height
+        return image, boxes, labels
+
+
+class RandomBrightness(object):
+    def __init__(self, delta=32):
+        assert delta >= 0.0
+        assert delta <= 255.0
+        self.delta = delta
+
+    def __call__(self, image, boxes=None, labels=None):
+        if np.random.randint(2):
+            delta = np.random.uniform(-self.delta, self.delta)
+            image += delta
+        return image, boxes, labels
+
+
+class RandomContrast(object):
+    def __init__(self, lower=0.5, upper=1.5):
+        self.lower = lower
+        self.upper = upper
+        assert self.upper >= self.lower, "Contrast upper must be >= lower."
+        assert self.lower >= 0, "Contrast lower must be non-negative."
+
+    def __call__(self, image, boxes=None, labels=None):
+        if np.random.randint(2):
+            alpha = np.random.uniform(self.lower, self.upper)
+            image *= alpha
+        return image, boxes, labels
